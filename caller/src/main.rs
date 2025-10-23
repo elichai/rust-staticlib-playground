9b781fc255fcb221d060376d9ea32f6e2a153e29
Elichai Turkel <elichai.turkel@gmail.com>
2025-10-23 13:06:15 +0300

    Make the rustlib more complicated and contain panics

--- caller/src/main.rs
+++ caller/src/main.rs
@@ -1,8 +1,18 @@
+use std::os::raw::c_char;
+
 unsafe extern "C" {
-    fn rustlib_add(left: u64, right: u64) -> u64;
+    fn rustlib_read_string_file(path: *const c_char) -> *mut c_char;
 }
 
 fn main() {
-    let result = unsafe { rustlib_add(2, 3) };
-    println!("Hello, world!, 2 + 3 = {}", result);
+    let this_file = file!();
+    let c_path = std::ffi::CString::new(this_file).expect("CString::new failed");
+    let content_ptr = unsafe { rustlib_read_string_file(c_path.as_ptr()) };
+    assert!(!content_ptr.is_null(), "Failed to read file");
+    let content_cstr = unsafe { std::ffi::CStr::from_ptr(content_ptr) };
+    let content_str = content_cstr.to_str().expect("Invalid UTF-8");
+    println!("Content of {}:\n{}", this_file, content_str);
+
+    assert_eq!(content_str, std::fs::read_to_string(this_file).unwrap());
+    println!("File content matches!");
 }
--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,4 +1,42 @@
+use std::{
+    ffi::{CStr, CString, c_char},
+    fs,
+    panic::catch_unwind,
+    ptr,
+};
+
 #[unsafe(no_mangle)]
-pub extern "C" fn rustlib_add(left: u64, right: u64) -> u64 {
-    left + right
+pub extern "C" fn rustlib_read_string_file(path: *const c_char) -> *mut c_char {
+    match catch_unwind(|| unsafe { read_c_string(path) }) {
+        Ok(ptr) => ptr,
+        Err(_) => {
+            eprintln!("rustlib_read_string_file: caught panic");
+            ptr::null_mut()
+        }
+    }
+}
+
+unsafe fn read_c_string(path: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(path) };
+    let path_str = match c_str.to_str() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("rustlib_read_string_file: invalid UTF-8 path: {}", e);
+            return ptr::null_mut();
+        }
+    };
+
+    let content = match fs::read_to_string(path_str) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!(
+                "rustlib_read_string_file: failed to read file {}: {}",
+                path_str, e
+            );
+            return ptr::null_mut();
+        }
+    };
+
+    let c_string = CString::new(content).expect("CString::new failed");
+    c_string.into_raw()
 }
